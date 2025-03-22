f508a6d1db479b995c2e9f3bb02b729876dfcee4
brogrammer232 <raymondmwaura232>
2025-03-22 17:53:04 -0400

    Split the main function into smaller functions.

--- server/main.c
+++ server/main.c
@@ -19,54 +19,43 @@ What this code does:
 #include <unistd.h>
 #include <sndfile.h>
 
-
 // Global constants.
 #define PORT "7777"
 #define AUDIO_FILE "song.wav"
 
-
-// Declaring functions.
-
+// Declaring functions (alphabetically arranged).
+void connect_client(int sockfd);
+int create_socket(struct addrinfo *res);
+struct addrinfo *getaddressinfo();
 
 // Main function.
 int main() {
 	// Declaring variables.
-	struct addrinfo hints, *res;
 	struct sockaddr_in client_addr;
 	SNDFILE *file;
 	SF_INFO sf_info;
-	int status, sockfd, client_sockfd;
+	int status, client_sockfd;
 	socklen_t client_addr_size;
 
-	// Setting up hints.
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
+	struct addrinfo *res = getaddressinfo();
+	int sockfd = create_socket(res);
+	connect_client(sockfd);
 
-	// Getting the linked list.
-	status = getaddrinfo(NULL, PORT, &hints, &res);
-	if (status != 0) {
-		fprintf(stderr, "getaddrinfo() Error: %s\n", gai_strerror(status));
-		exit(EXIT_FAILURE);
-	}
+	// Opening the audio file.
+	file = sf_open(AUDIO_FILE, SFM_READ, &sf_info);
 
-	// Creating a socket.
-	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if (sockfd == -1) {
-		perror("socket() error");
-		freeaddrinfo(res);
-		exit(EXIT_FAILURE);
-	}
+	// Freeing up memory and exiting.
+	close(sockfd);
+	close(client_sockfd);
+	exit(EXIT_SUCCESS);
+}
 
-	// Binding to the local IP.
-	status = bind(sockfd, res->ai_addr, res->ai_addrlen);
-	freeaddrinfo(res);
-	if (status == -1) {
-		perror("bind() error");
-		close(sockfd);
-		exit(EXIT_FAILURE);
-	}
+// Functions definition.
+void connect_client(int sockfd) {
+	/* This function connects to the client. */
+	struct sockaddr_in client_addr;
+	int client_sockfd, status;
+	socklen_t client_addr_size;
 
 	// Waiting for the client to connect.
 	printf("Waiting for the client to connect.\n");
@@ -85,13 +74,57 @@ int main() {
 		exit(EXIT_FAILURE);
 	}
 	printf("Successfully connected to the client.\n");
-
-	// Opening the audio file.
-	file = sf_open(AUDIO_FILE, SFM_READ, &sf_info);
+}
 
 
-	// Freeing up memory and exiting.
-	close(sockfd);
-	close(client_sockfd);
-	exit(EXIT_SUCCESS);
+int create_socket(struct addrinfo *res) {
+	/* This function creates and binds a socket using information in
+	the linked list argument. The file descriptor of the created socket
+	is returned. */
+	int sockfd, status;
+
+	// Creating a socket.
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sockfd == -1) {
+		perror("socket() error");
+		freeaddrinfo(res);
+		exit(EXIT_FAILURE);
+	}
+
+	// Binding to the local IP.
+	status = bind(sockfd, res->ai_addr, res->ai_addrlen);
+	freeaddrinfo(res);
+	if (status == -1) {
+		perror("bind() error");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+	// Returning the file descriptor.
+	return sockfd;
+}
+
+
+struct addrinfo *getaddressinfo() {
+	/* This function prepares everything needed to call getaddrinfo()
+	then calls it. It returns a pointer to the linked list created
+	by the function. */
+	struct addrinfo hints, *res;
+	int status;
+
+	// Setting up hints.
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE | AI_NUMERICSERV;
+
+	// Getting the linked list.
+	status = getaddrinfo(NULL, PORT, &hints, &res);
+	if (status != 0) {
+		fprintf(stderr, "getaddrinfo() Error: %s\n", gai_strerror(status));
+		exit(EXIT_FAILURE);
+	}
+
+	// Returning the linked list.
+	return res;
 }
